de828a26e11a9c08be0a6e7804b6b7431113a92f
v-whitetail <white.tail.millwork@gmail.com>
2024-01-23 22:37:26 -0500

    🦌 adding dialogue boxes 🦌

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
 #![allow(unused, dead_code)]
-use gcode_bandage::utils::*;
+use rfd::*;
 use anyhow::Result;
+use gcode_bandage::utils::*;
 
 fn main() -> Result<()> {
 
+    let files = FileDialog::new()
+        .set_title("Select PRG to process")
+        .add_filter("PRG", &["prg"])
+        .set_directory(std::env::current_dir()?)
+        .pick_files()
+        .expect("no files selected");
+
+    for file in files {
+        let result = process_file(&file);
+        if let Err(err) = result {
+            MessageDialog::new()
+                .set_title("Bandage Error")
+                .set_level(MessageLevel::Error)
+                .set_buttons(MessageButtons::Ok)
+                .set_description(err.to_string())
+                .show();
+        };
+    }
+
+    Ok(())
+}
+
+fn process_file(path: &std::path::PathBuf) -> Result<()> {
+
+    let source_stem = path.file_stem().expect("dialog read error");
+    let mut target_name = source_stem.to_os_string();
+    target_name.push("-tabs.prg");
+    let mut target_file = path.clone();
+    target_file.set_file_name(target_name);
+
     let config = Config::read("config.toml")?;
 
-    let gcode = std::fs::read_to_string("gcode.prg")?;
+    let gcode = std::fs::read_to_string(path)?;
 
     let mut dom = DOM::parse(&gcode, &config)?;
 
@@ -14,8 +45,7 @@ fn main() -> Result<()> {
 
     let gcode = dom.serialize();
 
-    std::fs::write("tabbed.prg", &gcode)?;
+    std::fs::write(target_file, &gcode)?;
 
     Ok(())
 }
-
